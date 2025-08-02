a0d3756368643786b8f2e0325d797d1af39f51be
jiseon723 <dighxxx12@gmail.com>
2025-08-02 15:38:02 +0900

    question detail

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,10 @@
+package com.mysite.sbb.answer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+}
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -11,13 +12,20 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/article/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         //조회한 질문 리스트(questionList)를 모델에 "questionList"라는 이름으로 추가-> 뷰에서 ${questionList}로 접근가능
         model.addAttribute("questionList", questionList);
-        return "question list";
+        return "question_List";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
